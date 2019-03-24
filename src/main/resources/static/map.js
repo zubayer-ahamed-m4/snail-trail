a1e9d011120019de14cba22f5640fef71406ce26
zubay <zubay@DESKTOP-FR0HQC8>
2019-03-24 07:46:22 +0600

    updated

--- src/main/resources/static/map.js
+++ src/main/resources/static/map.js
@@ -3,10 +3,19 @@ var marker;
 var prevMarker;
 var currentMarker;
 var directionDisplay;
+var directionDisplayArray = [];
 var directionService;
-var latestMarkers = [];
 var markerArray = [];
 var allLatLong = [];
+var latestMarkersLatLong = [];
+var wayPoints = [];
+var locationsId = [];
+var markerColor = "9A6324";
+var resourcePath;
+var locationTimeInfoWindow;
+var stepDisplay;
+var MAX_QUERY_LIMIT = 25;
+var MAX_DELAY = 3000;
 var style = [ {
 		featureType : "poi",
 		elementType : "labels",
@@ -20,36 +29,262 @@ var style = [ {
 			visibility : "off"
 		} ]
 	} ];
-var resourceUrl;
+
+
+
 
 // MAIN
 $(document).ready(function() {
-	resourceUrl = $('#resourceUrl').attr('href');
+	stepDisplay = new google.maps.InfoWindow;
+	resourcePath = $('#resourcePath').val();
 	initializeMap();
-	//generateControlDiv();
 
-	// TODO: Driver search functionality will work before go down
-	getAllLatLongOfDriver();
-	controlPosition(allLatLong);
+	//TODO: date validation
+//	var validationText = "";+
+//	$.validator.addMethod("enddate", function(value, element) {
+//		var datepair = $(element).data("datepair");
+//		if ($("input[data-datepair=" + datepair + "]").not(".con-enddate").val() !== "") {
+//			var otherdate = $("input[data-datepair=" + datepair + "]").not(".con-enddate");
+//			var otherdateid = otherdate.attr('id');
+//			$("span.error[for='" + otherdateid + "']").hide();
+//			var startdatevalue = otherdate.val();
+//			console.log()
+//			
+//			var timeDiff = Math.abs((moment(value, "DD-MMM-YYYY").toDate()).getTime() - (moment(startdatevalue, "DD-MMM-YYYY").toDate()).getTime());
+//			var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+//			if(diffDays >= 5) return false;
+//			return moment(startdatevalue, "DD-MMM-YYYY") <= moment(value, "DD-MMM-YYYY");
+//		}
+//		return true;
+//	}, "text");
 
-	var i = 0;
-	var markerInterval = setInterval(() => {
-		if(i > 1) latestMarkers.shift();
-		latestMarkers.push(allLatLong[i]);
-		controlPosition(latestMarkers);
+//	$('#mainform').on('submit', function(e){
+//		e.preventDefault();
+//		var formData = $(this).serializeArray();
+//		var methodType = $(this).attr('method');
+//		var submitUrl = $(this).attr('action');
+//
+//		if(validateFormData(formData) == false) return;
 
-		setTimeout(() => {
-			generateMarkers(i);
-			updatePreviousMarkerIcon(i - 1);
-			if (i > 0) generateDirection();
-			i++;
+//		$.ajax({
+//			url: submitUrl,
+//			type: methodType,
+//			data: formData,
+//			async: false,
+//			success: function(data) {
+//				//console.log({data});
+//				if(data.status.toLowerCase() == 'error') showMessage('alert-danger', data.message);
+//				if(data.driverLocations.length <= 0){
+//					showMessage('alert-danger', "No location found for this driver at specified date time range.");
+//					return;
+//				}
+				
+	
+		var lat = [51.507566, 51.507046, 51.507570, 51.507984, 51.507139, 51.506231, 51.505934, 51.505609, 51.507178, 51.505869, 51.503679, 51.505242, 51.506143, 51.507112];
+		var lng = [-0.127612, -0.125456, -0.125896, -0.125354, -0.123831, -0.124319, -0.125536, -0.126799, -0.127914, -0.130780, -0.136067, -0.137472, -0.135306, -0.132668];
+		var driverLocations = [];
+		for(var i = 0; i < lat.length; i++){
+			driverLocations.push({
+				latitude : lat[i],
+				longitude : lng[i],
+				locationDate : "location date",
+				locationTime : "location time",
+				driverId : i,
+				locationId : i,
+			})
+		}
+		
+		var data = {"driverLocations": driverLocations};
+		console.log(data);
+		
+		resetAll();
+		// TODO: check duplicate lat long
+		getAllLatLongOfDriver(data);
+		controlPosition(allLatLong);
 
-			// STOP DISPLAY INTERVAL LOGIC
-			if(i == allLatLong.length) stopDisplayInterval(markerInterval);
+		var OFFSET = 0;
+		var queryInterval = setInterval(() => {
+			var currentlyAllowedLatLongs = [];
+			for(var i = OFFSET; i < OFFSET + MAX_QUERY_LIMIT && i < allLatLong.length; i++){
+				currentlyAllowedLatLongs.push(allLatLong[i]);
+			}
 
-		}, 500);
-	}, 3000);
+			// CALL DIRECTION API
+			generateDirectionWithAllowedLatLongs(currentlyAllowedLatLongs);
+
+			OFFSET = (OFFSET-1) + MAX_QUERY_LIMIT;
+			//console.log("%cOFFSET NOW : " + OFFSET, "color: red");
+			var END_OF_PROCESS = false;
+			if(OFFSET >= allLatLong.length) {
+				END_OF_PROCESS = true;
+				clearInterval(queryInterval);
+			}
+
+			// DISPLAY MARKER
+			generateAndDisplayCurrentlyAllowedmarker(currentlyAllowedLatLongs, END_OF_PROCESS);
+			generateAndBindInfoWindowWithMarkers();
+			updateTimeControlDiv();
+			//if(END_OF_PROCESS == true) getLiveDriverLocation(formData, methodType, submitUrl);
+
+		}, MAX_DELAY);
+		
+		
+//
+//			},
+//			error: function(jqXHR, status, errorThrown) {
+//				showMessage('alert-danger', "Something went wrong....");
+//			}
+//		});
+//	});
+
 });
+
+
+var latestLatLong = [];
+// GET LIVE DRIVER LOCATION DATA
+function getLiveDriverLocation(formData, methodType, submitUrl){
+	var toDate = formData[4].value;
+	var toTime = formData[5].value == '' ? '23:59' : formData[5].value;
+	//console.log(toDate + " " + toTime);
+	if(toDate != '' && (Math.abs((moment(toDate + " " + toTime, "DD-MMM-YYYY hh:mm").toDate()).getTime() < new Date().getTime())) == 1) return;
+
+	// TODO: will be 3 second delay effect
+	var liveUpdateInterval = setInterval(() => {
+		console.log("%cCALLING NEXT DATA EACH 3 SECIOND", "color:red;")
+		$.ajax({
+			url: submitUrl,
+			type: methodType,
+			data: formData,
+			async: false,
+			success : function(data){
+				console.log({data});
+				// FILTER LATEST LOCATIONS
+				data.driverLocations.forEach(function(dl){
+					if(locationsId.indexOf(dl.locationId) < 0) {
+						var latestLocation = {
+							lat : Number(dl.latitude),
+							lng : Number(dl.longitude),
+							locationDate : dl.locationDate,
+							locationTime : dl.locationTime,
+							driverId : dl.driverId,
+							locationId : dl.locationId
+						}
+						allLatLong.push(latestLocation);
+						latestMarkersLatLong.push(latestLocation);
+					}
+				});
+
+			},
+			error : function(jqXHR, status, errorThrown){
+				showMessage('alert-danger', "Something went wrong....");
+			}
+		})
+
+	}, MAX_DELAY);
+
+	
+	
+}
+
+// UPDATE TIME CONTROL DIV
+function updateTimeControlDiv(){
+	$('.time-control-div').html(markerArray[markerArray.length - 1].title);
+}
+
+// GENERATE INFO WINDOW FOR CURRENTLY ALLOWED MARKER
+function generateAndBindInfoWindowWithMarkers(){
+	markerArray.forEach(function(marker){
+		locationTimeInfoWindow = new google.maps.InfoWindow();
+		google.maps.event.addListener(marker, 'click', function() {
+			locationTimeInfoWindow.setContent(marker.title);
+			locationTimeInfoWindow.open(map, marker);
+		});
+	})
+	
+}
+
+// GENERATE DIRECTION WITH CURRENTLY ALLOWED LAT LONGS
+function generateDirectionWithAllowedLatLongs(currentlyAllowedLatLongs){
+	if(currentlyAllowedLatLongs.length <= 1) return;
+
+	var startPosition = currentlyAllowedLatLongs[0];
+	var endPosition = currentlyAllowedLatLongs[currentlyAllowedLatLongs.length - 1];
+	var wayPoints = [];
+	for(var i = 0; i < currentlyAllowedLatLongs.length; i++){
+		if(i == 0 || i == (currentlyAllowedLatLongs.length - 1)) continue;
+		wayPoints.push({
+			location: new google.maps.LatLng(Number(currentlyAllowedLatLongs[i].lat), Number(currentlyAllowedLatLongs[i].lng)),
+			stopover: true
+		})
+	}
+
+//	console.log("%cSTART : ", "color: green"); console.log({startPosition});
+//	console.log("%cWAAYPOINTS : ", "color: green"); console.log({wayPoints});
+//	console.log("%cEND : " , "color: green"); console.log({endPosition});
+
+	directionService = new google.maps.DirectionsService;
+	directionDisplay = new google.maps.DirectionsRenderer();
+
+	directionService.route({
+		origin: startPosition,
+		destination: endPosition,
+		waypoints: wayPoints,
+		travelMode: google.maps.TravelMode.DRIVING
+	}, function(response, status) {
+		//console.log({response});
+		if (status === google.maps.DirectionsStatus.OK) {
+			directionDisplay.setDirections(response);
+		} else {
+			console.log('Directions request failed due to ' + status);
+		}
+	});
+	directionDisplay.setMap(map);
+	directionDisplay.setOptions( { preserveViewport : false, suppressMarkers: true, zoom : 5 } );
+	directionDisplayArray.push(directionDisplay);
+}
+
+
+
+// GENERATE AND DISPLAY CURRENTLY ALLOWD MARKER
+function generateAndDisplayCurrentlyAllowedmarker(currentlyAllowedLatLongs, END_OF_PROCESS) {
+	var image = {
+			url : resourcePath + 'm1.png',
+			origin: new google.maps.Point(0, 0)
+		}
+
+	for(var i = 0; i < currentlyAllowedLatLongs.length; i++){
+
+		//if(i == currentlyAllowedLatLongs.length - 1 && END_OF_PROCESS == false) continue;
+		if(i == currentlyAllowedLatLongs.length - 1) {
+			image = {
+					url : resourcePath + 'driver.png',
+					origin: new google.maps.Point(0, 0)
+				}
+		}
+
+		var marker = new google.maps.Marker({
+			position: currentlyAllowedLatLongs[i],
+			icon : image,
+			map: map,
+			title: currentlyAllowedLatLongs[i].locationDate + " @ " + currentlyAllowedLatLongs[i].locationTime
+		});
+
+		markerArray.push(marker);
+		updatePreviousMarkerIcon(i, currentlyAllowedLatLongs, marker);
+	}
+}
+
+// UPDATE PREVIOUS MARKER ICON
+var PREV_MARKER;
+function updatePreviousMarkerIcon(i, currentlyAllowedLatLongs, marker){
+	if(PREV_MARKER != undefined && i == currentlyAllowedLatLongs.length - 1) {
+		PREV_MARKER.setIcon({
+			path: google.maps.SymbolPath.CIRCLE,
+			scale: 0
+		});
+	}
+	if(i == currentlyAllowedLatLongs.length - 1) PREV_MARKER = marker;
+}
 
 // INITIALIZE MAP
 function initializeMap(){
@@ -67,18 +302,19 @@ function initializeMap(){
 	});
 }
 
-// GET ALL DRIVERS LAT/LON
-function getAllLatLongOfDriver(){
-	allLatLong = [
-		{lat: 53.408371, lng: -2.991573},
-		{lat: 53.810585, lng: -1.509032},
-		{lat: 53.602488, lng: -0.652473},
-		{lat: 53.143682, lng: -0.564621},
-		{lat: 52.479644, lng: -0.279101},
-		{lat: 51.4592483, lng: -0.139455},
-		{lat: 51.177729, lng: -1.215644},
-		{lat: 50.894480, lng: -3.357041}
-	];
+// GET ALL DRIVERLOCATION LAT/LON
+function getAllLatLongOfDriver(data){
+	data.driverLocations.forEach(function(driver){
+		allLatLong.push({
+			lat : Number(driver.latitude),
+			lng : Number(driver.longitude),
+			locationDate : driver.locationDate,
+			locationTime : driver.locationTime,
+			driverId : driver.driverId,
+			locationId : driver.locationId
+		})
+		locationsId.push(driver.locationId);
+	})
 }
 
 // CONTROL MAP POSITION TO CENTER
@@ -91,120 +327,36 @@ function controlPosition(latLangs){
 	map.fitBounds(latlngbounds);
 }
 
-// GENERATE CONTROL DIV AND PLACE IT TO MAP
-function generateControlDiv(){
-	var controlDiv = document.createElement("div");
-	controlDiv.classList.add("control-div");
-	controlDiv.innerHTML = uiControlDiv();
-	map.controls[google.maps.ControlPosition.LEFT_TOP].push(controlDiv);
+//VALIDATE FORM DATA
+function validateFormData(formData){
+	if (formData[0].value == undefined || formData[0].value == '') return false;
+	if (formData[2].value == undefined || formData[2].value == '') return false
+	if (formData[3].value == undefined || formData[3].value == '') return false;
+	if (formData[4].value == undefined || formData[4].value == '') return false
+	if (formData[5].value == undefined || formData[5].value == '') return false;
+	return true;
 }
 
-// GENERATE MARKER
-function generateMarkers(index){
-	var today = new Date();
-
-	var image = {
-		url : resourceUrl + "driver.png",
-		origin: new google.maps.Point(0, 0)
+// REMOVE ALL MARKER FROM MAP
+function removeAllMarkerAndClearMap(){
+	for (var i = 0; i < markerArray.length; i++) {
+		markerArray[i].setMap(null);
 	}
+}
 
-	marker = new google.maps.Marker({
-		position: allLatLong[index],
-		icon : image,
-		map: map,
-		title: today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate()
+// REMOVE ALL DIRECTIONS FROM MAP
+function removeDirectionsLine(){
+	if(directionDisplayArray == undefined || directionDisplayArray.length <= 0) return;
+	directionDisplayArray.forEach(function(direction){
+		direction.setMap(null);
 	});
+}
 
-	markerArray.push(marker);
-
-	if(index < 1) {
-		currentMarker = allLatLong[index]
-		return;
+// RESET ALL
+function resetAll(){
+	while(allLatLong.length > 0){
+		allLatLong.pop();
 	}
-
-	prevMarker = currentMarker;
-	currentMarker = allLatLong[index];
-}
-
-// UPDATE PREVIOUS MARKER ICON
-function updatePreviousMarkerIcon(index){
-	if(markerArray.length <= 0 || index < 0) return;
-
-	var image = {
-		url : resourceUrl + "m1.png",
-		origin: new google.maps.Point(0, 0)
-	}
-
-	markerArray[index].setIcon(image);
-}
-
-// GENERATE DIRECTION
-function generateDirection(){
-	setTimeout(() => {
-		directionDisplay = new google.maps.DirectionsRenderer;
-		directionService = new google.maps.DirectionsService;
-
-		directionService.route({
-			origin: prevMarker,
-			destination: currentMarker,
-			travelMode: google.maps.TravelMode.DRIVING
-		}, function(response, status) {
-			if (status === google.maps.DirectionsStatus.OK) {
-				directionDisplay.setDirections(response);
-			} else {
-				showError('Directions request failed due to ' + status);
-			}
-		});
-		directionDisplay.setMap(map);
-		directionDisplay.setOptions( { preserveViewport : true, suppressMarkers: true, zoom : 5 } );
-	}, 500);
-}
-
-//STOP DISPLAY
-function stopDisplayInterval(markerInterval){
-	clearInterval(markerInterval);
-	setTimeout(() => {
-		controlPosition(allLatLong);
-	}, 1000);
-}
-
-// CONTROL DIV
-function uiControlDiv(){
-	var bodyWidth = $('body').width();
-	var leftMargin = (bodyWidth - bodyWidth/2)-60;
-
-	return 	'<div class="time-control-div" style="margin-left: '+ leftMargin +'px;">01/01/2019 @ 12:34</div>'+
-			'<div class="driver-control-div">'+
-				'<h3>Show location history for ...</h3>'+
-				'<form class="form-horizontal snailtrailform">'+
-					'<div class="control-group">'+
-						'<label class="control-label" for="job-search" th:text="#{pod.label.search}">Driver : </label>'+
-						'<div class="controls">'+
-							'<div class="input-append">'+
-								'<input type="text" class="typeahead input-large" placeholder="Search ..." autocomplete="off"/>'+ 
-								'<span class="add-on"><i class="icon-search"></i></span>'+
-							'</div>'+
-						'</div>'+
-					'</div>'+
-					'<div class="control-group">'+
-						'<label class="control-label" for="expectedDispatchDate" th:text="#{hermes.dispatchdate.label}">From : </label>'+
-						'<div class="controls">'+
-							'<div class="input-append">'+
-								'<input class="input-datesize include con-futuredate required" type="text" /><span class="add-on"><i class="icon-calendar"></i></span> @ '+
-								'<input class="input-mini include con-timepicker required" type="text"/><span class="add-on"><i class="icon-time"></i></span>'+
-							'</div>'+
-						'</div>'+
-					'</div>'+
-					'<div class="control-group">'+
-						'<label class="control-label" for="expectedDispatchDate" th:text="#{hermes.dispatchdate.label}">To : </label>'+
-						'<div class="controls">'+
-							'<div class="input-append">'+
-								'<input class="input-datesize include con-futuredate required" type="text" /><span class="add-on"><i class="icon-calendar"></i></span> @ '+
-								'<input class="input-mini include con-timepicker required" type="text"/><span class="add-on"><i class="icon-time"></i></span>'+
-							'</div>'+
-						'</div>'+
-					'</div>'+
-					'<button type="submit" class="btn btn-primary pull-right snail-trail-submit-btn">Submit</button>'+
-				'</form>'+
-			'</div>';
+	removeAllMarkerAndClearMap();
+	removeDirectionsLine();
 }
